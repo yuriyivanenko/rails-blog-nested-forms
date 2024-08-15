b220359cff89dcf34a5d22314f0f544a08b013e2
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-15 13:56:40 -0400

    Pass all tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "default"
+  # config.order = "default"
   config.include Capybara::DSL
   
   DatabaseCleaner.strategy = :truncation
